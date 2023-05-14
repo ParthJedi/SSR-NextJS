bc67f933e67d177e920a3105cfed59bb828700a5
parth-spritle <91447862+parth-spritle@users.noreply.github.com>
2023-05-14 19:59:26 +0530

    data-fetching in Next.js v13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-export default function Home(): JSX.Element {
+export default async function Home() {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=8"
+  );
+  if (!res.ok) throw "Error loading posts";
+  const posts = await res.json();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Getting started with Next.js V13</h1>
         <p>Hello, Next.js!</p>
+      </div>
+      <div>
+        {posts.length &&
+          posts.map((post: any) => {
+            return (
+              <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
+            );
+          })}
+        <h3></h3>
       </div>
     </main>
   );
