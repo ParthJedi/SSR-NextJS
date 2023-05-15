cb3f797c7a6aa64f4d0900052255ec3b44f2b5fd
parth-spritle <91447862+parth-spritle@users.noreply.github.com>
2023-05-15 11:40:48 +0530

    interim commit

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,4 +1,6 @@
-async function Article({ params }: any) {
+import Link from "next/link";
+
+export default async function Article({ params }: any) {
   const { id } = params;
 
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
@@ -6,9 +8,13 @@ async function Article({ params }: any) {
 
   return (
     <>
-      <div>Article {id}</div>
+      <h1>{article.title}</h1>
+      <div>
+        <p>{article.body}</p>
+      </div>
+      <Link href="/">
+        <span>&larr; Go Back</span>
+      </Link>
     </>
   );
 }
-
-export default Article;
